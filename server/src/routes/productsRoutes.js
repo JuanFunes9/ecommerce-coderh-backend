84ab11a9356b43df2d4eab9e292e089ab72bec39
juanf <juan.funes.96@outlook.com>
2022-06-22 01:33:29 -0300

    MAKE IT BEETTER

--- server/src/routes/productsRoutes.js
+++ server/src/routes/productsRoutes.js
@@ -25,62 +25,86 @@ router.get( '/:id', ( req, res ) => {
 
 //3) Recibe y agrega un producto. Devuelve el producto agregado y su ID asignada (disponible para admins)
 router.post( '/', ( req, res ) => {
-  const product = req.body;
-  const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
-  const products = JSON.parse( read );
+  if( req.headers.admin ){
+    const product = req.body;
+    const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
 
-  const date = new Date();
-  product.timeStamp = date.toISOString().split('T')[0] + ' ' + date.toLocaleTimeString();
+    const date = new Date();
+    product.timeStamp = date.toISOString().split('T')[0] + ' ' + date.toLocaleTimeString();
 
-  const productsId = products.map( p => p.id );
-  product.id = Math.max( ...productsId ) + 1;
+    const productsId = products.map( p => p.id );
+    product.id = Math.max( ...productsId ) + 1;
 
-  products.push( product );
+    products.push( product );
 
-  fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
-  res.json( product );
+    fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
+    res.json( product );
+  }
+  else{
+    res.json({
+      error: -1,
+      desc: 'Ruta y metodo POST no autorizado.',
+    })
+  }
 });
 
 //4) Edita un producto segun su id: (disponible para admins)
 router.put( '/:id', ( req, res ) => {
-  //a)obtenemos el id y el poroducto:
-  const id = Number( req.params.id );
-  const product = req.body;
-  //b) asignamos el id y actualizamos el timeStamp:
-  const date = new Date();
-  product.timeStamp = date.toISOString().split('T')[0] + ' ' + date.toLocaleTimeString();
-  product.id = id;
-  //c) traemos el array de productos:
-  const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
-  const products = JSON.parse( read );
-  //d) buscamos el index del producto a editar:
-  const idx = products.findIndex( p => p.id == id );
+  if( req.headers.admin ){
+    //a)obtenemos el id y el poroducto:
+    const id = Number( req.params.id );
+    const product = req.body;
+    //b) asignamos el id y actualizamos el timeStamp:
+    const date = new Date();
+    product.timeStamp = date.toISOString().split('T')[0] + ' ' + date.toLocaleTimeString();
+    product.id = id;
+    //c) traemos el array de productos:
+    const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
+    //d) buscamos el index del producto a editar:
+    const idx = products.findIndex( p => p.id == id );
 
-  if( idx === -1 ){
-      res.send({  error :'El producto que desea editar no existe.' })
-  } else {
-      products.splice( idx, 1, product );
+    if( idx === -1 ){
+        res.send({  error :'El producto que desea editar no existe.' })
+    } else {
+        products.splice( idx, 1, product );
 
-      fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
-      res.json( product );
+        fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
+        res.json( product );
+    }
+  }
+  else{
+    res.json({
+      error: -1,
+      desc: 'Ruta y metodo PUT no autorizado.',
+    })
   }
 });
 
 //5) Elimina un producto segun su id: (disponible para admins)
 router.delete( '/:id', ( req, res ) => {
-  const id = req.params.id;
-  const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
-  const products = JSON.parse( read );
+  if( req.headers.admin ){
+    const id = req.params.id;
+    const read = fs.readFileSync( './src/productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
 
-  const idx = products.findIndex( p => p.id == id );
+    const idx = products.findIndex( p => p.id == id );
 
-  if( idx === -1 ){
-      res.send( 'El producto que desea eliminar no existe.' )
-  } else {
-      products.splice( idx, 1 );
+    if( idx === -1 ){
+        res.send( 'El producto que desea eliminar no existe.' )
+    } else {
+        products.splice( idx, 1 );
 
-      fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
-      res.json( `Se elimino el producto con id: ${ id }` );
+        fs.writeFileSync( './src/productos.txt', JSON.stringify( products, null, '\t' ) );
+        res.json( `Se elimino el producto con id: ${ id }` );
+    }
+  }
+  else{
+    res.json({
+      error: -1,
+      desc: 'Ruta y metodo DELETE no autorizado.',
+    })
   }
 });
 
